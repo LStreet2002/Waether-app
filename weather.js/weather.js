512e5debc724a718e2f90b49ca9e23a45ada9b8b
Lewis Street <lstreet@salisbury6c.ac.uk>
2019-11-22 10:41:29 +0000

    celscius

--- weather.js/weather.js
+++ weather.js/weather.js
@@ -7,7 +7,7 @@ var weather = document.querySelector('.weather');
 
 
 button.addEventListener('click', async function fetcher() {
-    var response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputcity.value + '&APPID=f7b2e4417287ec1a07110967f2048dcc');
+    var response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputcity.value + '&units=metric&APPID=f7b2e4417287ec1a07110967f2048dcc');
     var variable = await response.json()
     console.log(variable)
 
@@ -17,7 +17,7 @@ button.addEventListener('click', async function fetcher() {
     var weatherReading = variable['weather'][0]['description']
 
     city.innerHTML = nameReading
-    temperature.innerHTML = temperatureReading
+    temperature.innerHTML = temperatureReading + 'celscius'
     weather.innerHTML = weatherReading
 }
 )
